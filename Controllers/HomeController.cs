037bc581fcbd1658b3e2d443b0aeb55a0770880f
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-11-24 19:17:30 +0600

    Single And Multiple File uploads

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using Files.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace Files.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _hostingEnvironment;
+        public string _dir;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostingEnvironment)
         {
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+            _dir = _hostingEnvironment.ContentRootPath;
         }
 
         public IActionResult Index()
@@ -23,15 +29,37 @@ namespace Files.Controllers
             return View();
         }
 
+        public IActionResult SingleFile(IFormFile file)
+        {
+
+            using (var fileStream = new FileStream(Path.Combine(_dir,"file.jpg"), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult MultipleFiles(IEnumerable<IFormFile> files)
+        {
+            int i = 0;
+            foreach(var file in files)
+            {
+                using (var fileStream = new FileStream(Path.Combine(_dir, $"file{i}.jpg"), FileMode.Create, FileAccess.Write))
+                {
+                    file.CopyTo(fileStream);
+                }
+                i++;
+            }
+            
+            return RedirectToAction("Index");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult SingleFile(IFormFile file)
-        {
-            return RedirectToAction("Index");
-        }
+       
     }
 }
